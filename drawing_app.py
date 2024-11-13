429c559584c7ded6dffabb8c6f2e3761e49efc5b
mixupp83 <161758839+mixupp83@users.noreply.github.com>
2024-11-13 15:11:18 +0400

    Функционал - выбор размера кисти из списка

--- drawing_app.py
+++ drawing_app.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from tkinter import colorchooser, filedialog, messagebox
 from PIL import Image, ImageDraw
 
-
 class DrawingApp:
     def __init__(self, root):
         self.root = root
@@ -14,14 +13,14 @@ class DrawingApp:
         self.canvas = tk.Canvas(root, width=600, height=400, bg='white')
         self.canvas.pack()
 
-        self.setup_ui()
-
         self.last_x, self.last_y = None, None
         self.pen_color = 'black'
+        self.brush_size = 1  # Изначальный размер кисти
+
+        self.setup_ui()
 
         self.canvas.bind('<B1-Motion>', self.paint)
         self.canvas.bind('<ButtonRelease-1>', self.reset)
-
 
     def setup_ui(self):
         control_frame = tk.Frame(self.root)
@@ -36,16 +35,19 @@ class DrawingApp:
         save_button = tk.Button(control_frame, text="Сохранить", command=self.save_image)
         save_button.pack(side=tk.LEFT)
 
-        self.brush_size_scale = tk.Scale(control_frame, from_=1, to=10, orient=tk.HORIZONTAL)
-        self.brush_size_scale.pack(side=tk.LEFT)
+        # Выпадающий список для выбора размера кисти
+        self.brush_size_var = tk.IntVar(value=self.brush_size)
+        sizes = [1, 2, 5, 10]
+        brush_size_menu = tk.OptionMenu(control_frame, self.brush_size_var, *sizes, command=self.update_brush_size)
+        brush_size_menu.pack(side=tk.LEFT)
 
     def paint(self, event):
         if self.last_x and self.last_y:
             self.canvas.create_line(self.last_x, self.last_y, event.x, event.y,
-                                    width=self.brush_size_scale.get(), fill=self.pen_color,
+                                    width=self.brush_size, fill=self.pen_color,
                                     capstyle=tk.ROUND, smooth=tk.TRUE)
             self.draw.line([self.last_x, self.last_y, event.x, event.y], fill=self.pen_color,
-                           width=self.brush_size_scale.get())
+                           width=self.brush_size)
 
         self.last_x = event.x
         self.last_y = event.y
@@ -69,12 +71,13 @@ class DrawingApp:
             self.image.save(file_path)
             messagebox.showinfo("Информация", "Изображение успешно сохранено!")
 
+    def update_brush_size(self, size):
+        self.brush_size = int(size)
 
 def main():
     root = tk.Tk()
     app = DrawingApp(root)
     root.mainloop()
-
 
 if __name__ == "__main__":
     main()
